d197936e1b2f942dbe9cd7e71497ff0810dd307c
negineri <negineri@sfc.wide.ad.jp>
2022-11-24 02:14:00 +0900

    feat: detecting moving objects

--- src/main1/pixi.ts
+++ src/main1/pixi.ts
@@ -53,8 +53,9 @@ let nextMs = 0;
 const intervalMs = 400;
 const texts: PIXI.Container[] = [];
 let scene = 0;
-let flags = { tracking: true, detectCount: 0 };
+let flags = { tracking: true, detect: false };
 let centralText: PIXI.Container;
+let pScale: number[] = [];
 
 type DataStore = {
   text: PIXI.Container;
@@ -67,7 +68,7 @@ const setupCamera = async (
   ticker: PIXI.Ticker,
   flags: {
     tracking: boolean;
-    detectCount: number;
+    detect: boolean;
   }
 ) => {
   const devices = (await navigator.mediaDevices.enumerateDevices())
@@ -118,44 +119,30 @@ const setupCamera = async (
       if (!flags.tracking) {
         return;
       }
-      flags.detectCount = 0;
       bufferCtx.drawImage(video, 0, 0);
-      const src = cv.imread(buffer);
-      let gray = new cv.Mat();
-      cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-      let faces = new cv.RectVector();
-      let eyes = new cv.RectVector();
-      let msize = new cv.Size(0, 0);
-      faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
-      for (let i = 0; i < faces.size(); ++i) {
-        let roiGray = gray.roi(faces.get(i));
-        let roiSrc = src.roi(faces.get(i));
-        let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-        let point2 = new cv.Point(
-          faces.get(i).x + faces.get(i).width,
-          faces.get(i).y + faces.get(i).height
+      let imageData = bufferCtx.getImageData(0, 0, buffer.width, buffer.height);
+      let nScale: number[] = [];
+      let diffScore = 0;
+      for (var i = 0; i < imageData.data.length / 4; i++) {
+        const scale = Math.floor(
+          (imageData.data[i * 4] +
+            imageData.data[i * 4 + 1] +
+            imageData.data[i * 4 + 2]) /
+            3 /
+            16
         );
-        cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
-        eyeCascade.detectMultiScale(roiGray, eyes);
-        for (let j = 0; j < eyes.size(); ++j) {
-          let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
-          let point2 = new cv.Point(
-            eyes.get(j).x + eyes.get(j).width,
-            eyes.get(j).y + eyes.get(j).height
-          );
-          cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
-          flags.detectCount++;
+        if (pScale.length === imageData.data.length / 4) {
+          diffScore += (pScale[i] - scale) ** 2;
         }
-        roiGray.delete();
-        roiSrc.delete();
+        pScale[i] = scale;
       }
-
-      cv.imshow(canvas, src);
-      // OpenCV.jsはemscriptenでできていて、C++の世界のオブジェクトは自動的に破棄されないため、データ構造を使ったら自分で破棄する必要がある
-      src.delete();
-      gray.delete();
-      faces.delete();
-      eyes.delete();
+      console.log(diffScore + " / " + (imageData.width * imageData.height) / 4);
+      if (diffScore > (imageData.width * imageData.height) / 4) {
+        flags.detect = true;
+      } else {
+        flags.detect = false;
+      }
+      canvasCtx.putImageData(imageData, 0, 0);
       cameraTexture.update();
     });
   };
@@ -200,10 +187,9 @@ function draw() {
     flags.tracking = true;
   } else if (scene == 1) {
     // スタンバイ
-    console.log(flags.detectCount);
-    if (flags.detectCount >= 2) {
+    if (flags.detect) {
       scene = 2;
-      flags.tracking = false;
+      // flags.tracking = false;
       const tl = GSAP.timeline();
       tl.to(store.text, {
         ease: "power2.out",
